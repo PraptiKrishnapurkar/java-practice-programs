2f2b08cfc8775e9d567c2f58fbba27528653f70e
Praptii <praptikrishnapurkar01@gmail.com>
2026-05-26 19:56:09 +0530

    Add files via upload

---
+++ ContainExample.java
@@ -0,0 +1,13 @@
+public class ContainsExample {
+    public static void main(String[] args) {
+
+        String str = "Welcome to College";
+
+        if(str.contains("College")) {
+            System.out.println("String contains College");
+        } else {
+            System.out.println("Not found");
+        }
+
+    }
+}
