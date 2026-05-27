82ebf54bf7a6fc9dcefe2a058f1ccd2f0b3c8289
Praptii <praptikrishnapurkar01@gmail.com>
2026-05-27 21:23:28 +0530

    Add files via upload

---
+++ classperson.java
@@ -0,0 +1,22 @@
+//WAP using class person to display name and age
+class Person 
+{
+  string name;
+int age;
+}//class person
+class myprogramperson {
+  public static void main(String[] args) {
+    person p1=new Person();  //create object p1
+    Person p2=new Person();  //create object p2
+    p1.name="modi";
+    p1.age=71;
+    p2.name="palak";
+    p2.age=80;
+    System.out.println("p1.name="+p1.name);
+    System.out.println("p2.name="+p2.name);
+    System.out.println("p1.age="+p1.age);
+    System.out.println("p1.age="+p2.age);
+}//main()
+}
+
+    
